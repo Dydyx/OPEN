aae55814fc4385a1cddae9280c3b4387e8c7c5bb
brebant <mathieu3.brebant@gmail.com>
2018-11-27 09:50:42 +0100

    WebController

--- projet-voiture/src/main/java/com/example/controller/WebController.java
+++ projet-voiture/src/main/java/com/example/controller/WebController.java
@@ -3,6 +3,8 @@
  */
 package com.example.controller;
 
+import java.util.List;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,9 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.model.Voiture;
 import com.example.repo.VoitureRepository;
+import com.example.service.impl.CalculImpl;
+import com.example.service.impl.CalculImpl2;
+import com.example.service.impl.CalculImpl3;
+
 
 /**
- * @author Dylan
+ * 
  *
  */
 @Controller
@@ -24,6 +30,15 @@ public class WebController {
 	
 	@Autowired
 	VoitureRepository repository;
+	
+	@Autowired
+	CalculImpl calculService;
+	
+	@Autowired
+	CalculImpl2 calculService2;
+	
+	@Autowired
+	CalculImpl3 calculService3;
 	
 	org.slf4j.Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -84,4 +99,73 @@ public class WebController {
 		
 		return "find";
 	}
+	
+	@RequestMapping("/searchmarquewithshortname")
+	public String fetchDatasearchMarqueWithShortName(@RequestParam("marque") String marque, Model model){
+		String result = "";
+		
+		for(Voiture cust: repository.searchMarqueWithShortName(marque)){
+			result += cust.toString() + "<br>"; 
+		}
+		
+		model.addAttribute("result", result);
+		
+		return "find";
+	}
+	
+	@RequestMapping("/findall")
+	public String findAll(Model model){
+		String result = "";
+		
+		for(Voiture cust : repository.findAll()){
+			result += cust.toString() + "<br>";
+		}
+		model.addAttribute("result", result);
+		
+		return "find";
+	}
+	
+	@RequestMapping("/calculMoyenne")
+	public String calculMoyenne(Model model){
+		
+		double moyenne = calculService.calculMoyenne();
+		
+		model.addAttribute("moyenne", moyenne);
+		
+		return "moyenne";
+	}
+	
+	@RequestMapping("/deuxRecentes")
+	public String calculDeuxPlusRecentes(Model model){
+		
+		List<Voiture> deuxRecentes = calculService2.deuxRecentes();
+		
+		String result = "";
+		
+		for(Voiture cust : repository.findAll()){
+			result += cust.toString() + "<br>";
+		}
+		
+		model.addAttribute("deuxRecentes", deuxRecentes);
+		
+		return "deuxRecentes";
+	}
+	
+	@RequestMapping("/deuxAnciennes")
+	public String calculDeuxPlusAnciennes(Model model){
+		
+		List<Voiture> deuxAnciennes = calculService3.deuxAnciennes();
+		
+		String result = "";
+		
+		for(Voiture cust : repository.findAll()){
+			result += cust.toString() + "<br>";
+		}
+		
+		model.addAttribute("deuxAnciennes", deuxAnciennes);
+		
+		return "deuxAnciennes";
+	}
+	
 }
+
